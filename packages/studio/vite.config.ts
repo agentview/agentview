968a948ae48efaed82f1972e4b988c0b10778ef8
Andrzej Dabrowski <a.r.dabrowski@gmail.com>
2025-10-17 16:38:10 +0200

    wip

--- packages/studio/vite.config.ts
+++ packages/studio/vite.config.ts
@@ -1,9 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import mdx from '@mdx-js/rollup'
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(), 
+    tsconfigPaths(), // for using "~" in imports (taken from tsconfigPaths)
+    mdx() // for testing the tailwindcss-typography plugin
+  ],
   server: {
     port: 1989,
   },
