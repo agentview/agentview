7036d2429161130864bcdf0ee7ce3cc9318f56ad
Andrzej Dabrowski <a.r.dabrowski@gmail.com>
2025-10-31 11:06:06 +0100

    wip

--- packages/studio/src/index.tsx
+++ packages/studio/src/index.tsx
@@ -4,8 +4,8 @@ export * from "./types";
 
 // export type * from "./types";
 
-// export * from "./lib/shared/apiTypes";
+export * from "./lib/shared/apiTypes";
 // export type * from "./lib/shared/apiTypes"; // fixme: this doesn't work
 
-export type { RunBody } from "./lib/shared/apiTypes";
-export { RunBodySchema } from "./lib/shared/apiTypes";
+// export type { RunBody } from "./lib/shared/apiTypes";
+// export { RunBodySchema } from "./lib/shared/apiTypes";
