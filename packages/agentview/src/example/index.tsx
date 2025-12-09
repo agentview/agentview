7e4dabcdc8318cbc43c320324195e1dded7e8a44
Andrzej Dabrowski <a.r.dabrowski@gmail.com>
2025-12-09 18:46:54 +0100

    wip

--- packages/agentview/src/example/index.tsx
+++
@@ -1,8 +0,0 @@
-import "agentview/styles.css";
-import { renderStudio } from "agentview";
-import agentviewConfig from "@examples/ts-basic/studio/agentview.config";
-
-renderStudio(
-    document.getElementById("agentview"), 
-    agentviewConfig
-);
--- packages/agentview/src/exampleLoader.tsx
+++ packages/agentview/src/exampleLoader.tsx
@@ -1,5 +1,7 @@
 import "./styles.css";
 import { renderStudio } from "agentview";
+
+// @ts-ignore
 import agentviewConfig from "@examples/ts-basic/studio/agentview.config";
 
 renderStudio(
