9b1c49e6fe3c624e54ee45d5a8bd352909705a3a
Andrzej Dabrowski <a.r.dabrowski@gmail.com>
2026-01-09 15:04:25 +0100

    wip

--- apps/webapp/app/routes/logout.tsx
+++ apps/webapp/app/routes/logout.tsx
@@ -1,12 +1,15 @@
 import { redirect, data } from "react-router";
 import type { Route } from "./+types/logout";
 import { authClient } from "~/authClient";
+import { queryClient } from "~/queryClient";
 
 export async function clientLoader({ request }: Route.LoaderArgs) {
   const response = await authClient.signOut();
   if (response.error) {
     throw data(response.error.message ?? "Logout failed", { status: response.error.status })
   }
+
+  queryClient.clear();
 
   return redirect('/login');
 }
